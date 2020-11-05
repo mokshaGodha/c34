0e8d3da7fd9b18dd615e944de71e34b83f5e7d4d
mokshaGodha <68106669+mokshaGodha@users.noreply.github.com>
2020-11-05 15:11:59 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+var ball;
+var db,ballP;
+
+function setup(){
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+
+    db=firebase.database()
+    ballP=db.ref("ball/position")
+    ballP.on("value", readPosition)
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function changePosition(x,y){
+    
+    ballP.set({ 
+        x:ball.x+x, 
+        y:ball.y+y
+    })
+    
+}
+
+function readPosition(data){
+    var position=data.val()
+    ball.x=position.x;
+    ball.y=position.y;
+}
